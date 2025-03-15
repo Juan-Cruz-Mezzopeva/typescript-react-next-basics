9f2ce66e4add94418c3f8adc0de982aa6cc6065d
Juan Cruz <mezzopevajuancruz@gmail.com>
2025-03-14 22:33:34 -0300

    tipado de props en tres formas de escribir funciones distintas pero que todas hacen lo mismo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,34 @@ type ButtonProps = {
 };
 
 
+// 1RA FORMA DE ESCRIBIR FUNCIONES PASANDO PROPS USANDO TIPOS DE DATOS CON TYPE 
 
-function Button(props: ButtonProps) {
-  const {text, subtitle, color} = props;
 
-  return (
-    <button type="button">
-      {text}
-      <br />
-      {subtitle}
-      <br />
-      {color}
-    </button>
-  )
+
+// function Button(props: ButtonProps) {
+//   const {text, subtitle, color} = props;
+
+//   return (
+//     <button type="button">
+//       {text}
+//       <br />
+//       {subtitle}
+//       <br />
+//       {color}
+//     </button>
+//   )
+// }
+
+// 2DA FORMA DE ESCRIBIR FUNCIONES PASANDO PROPS USANDO TIPOS DE DATOS CON TYPE 
+
+// const Button = ({text}:ButtonProps) => {
+//   return <button>{text}</button>
+// }
+
+// 3RA FORMA DE ESCRIBIR FUNCIONES PASANDO PROPS USANDO TIPOS DE DATOS CON TYPE 
+// React.FC es "React funcional component" es 
+const Button: React.FC<ButtonProps> = ({text, color, subtitle}) => {
+  return <button>{text}</button>
 }
 
 
