e48877826dfc4ed35406fe9d22a85ed4521a2bcd
Juan Cruz <mezzopevajuancruz@gmail.com>
2025-03-14 21:53:42 -0300

    paso de props por datos estructurados dentro de la funcion con props opcionales  y obligatorias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
-function Button() {
+type ButtonProps = {
+  text: string;
+  subtitle?: string;
+  color?: string;
+};
+
+
+
+function Button(props: ButtonProps) {
+  const {text, subtitle, color} = props;
+
   return (
-    <button type="button"> Click me</button>
+    <button type="button">
+      {text}
+      <br />
+      {subtitle}
+      <br />
+      {color}
+    </button>
   )
 }
 
@@ -8,7 +24,7 @@ function Button() {
 const page = () => {
   return (
     <div>
-      <Button/ >
+      <Button text="hello word" subtitle="buttom"  color="#00ffff"/ >
     </div>
   )
 }
