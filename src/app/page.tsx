c3c2cab5c2bc7ac47ccb7937199dae7799917b2e
Juan Cruz <mezzopevajuancruz@gmail.com>
2025-03-17 22:36:08 -0300

    Uso de Union Types o union de tipos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,31 @@
+type Color = "red" | "blue" | "green" | "yellow" | "black" | "white" | "gray";
+
 type ButtonProps = {
   text: string;
   subtitle?: string;
-  color?: string;
+  color?: Color;
+  backgruondColor?: Color ;
+  sizes: 30 | 40 | 50
 };
 
 
 // 1RA FORMA DE ESCRIBIR FUNCIONES PASANDO PROPS USANDO TIPOS DE DATOS CON TYPE 
 
+function Button(props: ButtonProps) {
+  const {text, subtitle, color} = props;
 
-
-// function Button(props: ButtonProps) {
-//   const {text, subtitle, color} = props;
-
-//   return (
-//     <button type="button">
-//       {text}
-//       <br />
-//       {subtitle}
-//       <br />
-//       {color}
-//     </button>
-//   )
-// }
-
-// 2DA FORMA DE ESCRIBIR FUNCIONES PASANDO PROPS USANDO TIPOS DE DATOS CON TYPE 
-
-// const Button = ({text}:ButtonProps) => {
-//   return <button>{text}</button>
-// }
-
-// 3RA FORMA DE ESCRIBIR FUNCIONES PASANDO PROPS USANDO TIPOS DE DATOS CON TYPE 
-// React.FC es "React funcional component" es 
-const Button: React.FC<ButtonProps> = ({text, color, subtitle}) => {
-  return <button>{text}</button>
+  return (<button type="button">{text}</button>
+  )
 }
-
 
 const page = () => {
   return (
     <div>
-      <Button text="hello word" subtitle="buttom"  color="#00ffff"/ >
+      <Button text="hello word" 
+      backgruondColor="blue"
+      color="red"
+      / >
+
     </div>
   )
 }
