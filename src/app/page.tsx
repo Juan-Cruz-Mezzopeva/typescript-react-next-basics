4313b94092ad3e1fc4fa46ea02a085eb06db85ea
Juan Cruz <mezzopevajuancruz@gmail.com>
2025-03-19 13:50:16 -0300

    uso de Defeult Props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,23 @@
   "use client"
 
-import { Dispatch, useState, SetStateAction } from "react";
 
   type ButtonProps = {
-    setCount: Dispatch<SetStateAction<number>>
+    title: string
   };
 
  
-  function Button({setCount}: ButtonProps) {
+  function Button({title = "Text Here!!"}: ButtonProps) {
     return (
-    <button onClick={() => setCount((contador) => contador + 1)}> Incrementar </button>
+    <button> {title }</button>
     )
   }
 
-//el boton espera el evento setCount
-// se ejecuta el evento setCount en onClick el cual recibe el evento 
-
   function Page() {
 
-    const [count, setCount] = useState(0)
 
     return (
       <div>
-        <h1>{count}</h1>
         <Button 
-          setCount={setCount}
         />
       </div>
     )
