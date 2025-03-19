f0c645f305c16ce1f51f50b6fe68fd77a896549f
Juan Cruz <mezzopevajuancruz@gmail.com>
2025-03-19 18:29:32 -0300

    Spreed Operator para atributos de etiquetas como props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,38 @@ import { ComponentPropsWithoutRef } from "react";
 
   type ButtonProps = ComponentPropsWithoutRef<"button"> ;
 
-  //con ComponenProps (que puede ser simple con o sin ref) podemos pasar 
-  // todos los props que vienen a partir de etiquetas sin la necesidad de escribirlas todas, por ejemplo ese onClick
-  // tener en cuenta que hay que escribir de que etiqueta quiero los atributos como props
-  
-  function Button(props: ButtonProps) {
+  function Button({onClick, ...rest}: ButtonProps) {
+    
+    const handleClick = () => {
+      if(onClick) {
+        alert('Cliked')
+      }
+    }
+
     return (
-      <button {...props}>
-        {props.children || "Haz clic aquí"}
+      <button 
+      onClick={handleClick} {...rest}>
+        Button        
       </button>
     );
   }
   
   function Page() {
-    const handleClick = () => {
-      alert("¡Botón clickeado!");
-    };
-  
+
     return (
       <div>
-        <Button onClick={handleClick} disabled={false}>
+        <Button 
+        style={{
+          color: "transparent"
+        }}
+        onClick={() => {
+          alert("hello world!!")
+        }}
+        disabled={true}>
           Mi Botón
         </Button>
       </div>
     );
   }
-  
+  // 
   export default Page;
